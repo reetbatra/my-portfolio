a81245d927b0d1affb287cd1d03de347a43dd28e
reetbatra <reetbatra25@gmail.com>
2024-07-02 20:18:40 +0530

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
 		<main className='flex min-h-screen flex-col items-center justify-between '>
 			<Hero />
 			<Skills />
-
 			<Projects />
 			<POW />
 			<ArticleComponent />
